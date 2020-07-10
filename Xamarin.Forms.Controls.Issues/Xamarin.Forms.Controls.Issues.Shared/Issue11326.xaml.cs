9e062ade775836c5bfd3fc89e52cb0961379d7a0
Brian Macomber <brian@agspectrum.com>
2020-07-10 12:00:16 -0500

    Add 11326 Test page

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11326.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 11326, "[Bug] [UWP] ScrollView issue within RefreshView", PlatformAffected.UWP)]
+	public partial class Issue11326 : ContentPage
+	{
+		public Issue11326()
+		{
+#if APP
+			InitializeComponent();
+#endif
+		}
+	}
+}
